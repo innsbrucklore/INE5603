54a28144ba244158299022b105efc97e561f5c6a
innsbrucklore <fafbatalha@gmail.com>
2025-09-01 07:25:44 -0300

    back

---
+++ Relacionais/Relacionais.py
@@ -0,0 +1,35 @@
+'''
+num1 = int(input('Insira o num1: '))
+num2 = int(input('Insira o num2: '))
+print(bool(num2 > num1))
+'''
+'''
+num1 = int(input('Insira o num1: '))
+num2 = int(input('Insira o num2: '))
+print(bool(num1 > 0 and num2 > 0))
+'''
+'''
+num1 = float(input('Insira o num1: '))
+print(bool(0 < num1 < 1))
+'''
+'''
+num1 = int(input('Insira o num1: '))
+num2 = int(input('Insira o num2: '))
+num3 = int(input('Insira o num3: '))
+print(bool(num1 != num2 and num1 != num3 and num2 != num3))
+'''
+'''
+num1 = int(input('Insira o num1: '))
+num2 = int(input('Insira o num2: '))
+num3 = int(input('Insira o num3: '))
+print(bool(num1 < num2 < num3))
+'''
+'''
+num1 = int(input('Insira o num1: '))
+print(bool(num1 % 2 == 0))
+'''
+'''
+num = int(input('Insira um número: '))
+penultimo = (abs(num) // 10) % 10   
+print(penultimo % 2 == 1)
+'''
